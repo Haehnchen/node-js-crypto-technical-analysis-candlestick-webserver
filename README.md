501e8edb458232b914ae486f8898706d4c1c592e
Daniel Espendiller <daniel@espendiller.net>
2018-05-26 15:23:29 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# Candlestick Technical Analysis Http Server
+
+## Tools
+
+- node.js
+- technicalindicators lib
+
+## Start
+
+```
+npm install
+node index.js
+```
+
+Or change port listen on
+
+```
+node index.js 8080
+```
+
+## Start
+
+Download some candles from live market and post them to the webserver
+
+Some Urls:
+```
+https://api.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist?limit=500
+
+```
+
+Candles must be formatted as the common why in a numeric array whereas newest candles must be given first oldest last
+        
+```
+{
+[
+    1527335700000, // micro timestamp
+    7532.5, // open
+    7532.5, // close
+    7532.5,  // high
+    7532.5,  // low
+    1.02278819 // volume
+],
+[
+    1527340320000,
+    7521.2,
+    7522.9,
+    7522.9,
+    7521.2,
+    0.59021799
+]
+}
+```
+
+## Indicators
+
+### EMA
+
+```
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/ema"
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/ema?period=200"
+```
+
+### SMA
+
+```
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/sma"
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/sma?period=200"
+
+```
+
+### Bollinger Bands
+
+```
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/bollinger_bands"
+```
+
+### Stochastic RSI
+
+```
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/stoch_rsi"
+```
+
+### Stochastic
+
+```
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/stoch"
+```
+
+### MACD
+
+```
+curl -H "Content-Type: application/json" --data @candles.json "http://127.0.0.1:8080/macd"
+```
---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const ti = require('technicalindicators');
+
+const app = express()
+const port = process.argv[2] || 8080
+
+app.use(express.json())
+
+app.post('/:indicator', (req, response) => {
+    let content = {};
+
+    switch (req.params.indicator) {
+        case 'ema':
+            content.result = ti.EMA.calculate({
+                period : req.query.period || 9,
+                values: formatCandles(req.body).close
+            })
+
+            break;
+        case 'sma':
+            content.result = ti.EMA.calculate({
+                period : req.query.period || 9,
+                values: formatCandles(req.body).close
+            })
+
+            break;
+        case 'bollinger_bands':
+            content.result = ti.BollingerBands.calculate({
+                period : req.query.period || 30,
+                values: formatCandles(req.body).close, stdDev: 2
+            })
+
+            break;
+        case 'macd':
+            content.result = ti.MACD.calculate({
+                fastPeriod: 12,
+                slowPeriod: 26,
+                signalPeriod : 9,
+                values: formatCandles(req.body).close
+            })
+
+            break;
+        case 'stoch_rsi':
+            content.result = ti.StochasticRSI.calculate({
+                rsiPeriod: 14,
+                stochasticPeriod: 14,
+                kPeriod: 3,
+                dPeriod: 3,
+                values: formatCandles(req.body).close
+            })
+
+            break;
+        case 'stoch':
+            let candles = formatCandles(req.body);
+
+            content.result = ti.Stochastic.calculate({
+                high: candles.high,
+                low: candles.low,
+                close: candles.close,
+                period: 14,
+                signalPeriod: 3
+            })
+
+            break;
+        default:
+            content.error = 'unknown indicator'
+    }
+
+    response.setHeader('Content-Type', 'application/json');
+    response.send(content)
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
+
+function formatCandles(candles) {
+    let format = {
+        'open': [],
+        'close': [],
+        'high': [],
+        'low': [],
+        'vol': [],
+    }
+
+    // reverse candles as oldest candle must given first 'technicalindicators' lib
+    candles.reverse().forEach(candle => {
+        format.open.push(candle[1])
+        format.close.push(candle[2])
+        format.high.push(candle[3])
+        format.low.push(candle[4])
+        format.vol.push(candle[5])
+    })
+
+    return format;
+}
